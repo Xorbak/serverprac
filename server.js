883618ade82726b5c3393a3d6b30216c65b732c7
dewald.labuschagne <dewaldstar@gmail.com>
2023-02-10 17:58:45 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+require("dotenv").config();
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3", "user4"] });
+});
+
+app.get("/quote", (req, res) => {
+  const options = {
+    method: "GET",
+    url: `${process.env.REACT_APP_CARTOON}`,
+  };
+
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
+});
