7ba337a27d285e1252d419dd76b2630fb388f736
dewald.labuschagne <dewaldstar@gmail.com>
2023-02-22 12:41:19 +0200

    Added calls to cartoon quotes DB

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.get("/quote", (req, res) => {
     res.json(result.data);
   });
 });
+//-----------------------------------------------------------------------AI apis
 // image generation
 app.get("/image", (req, res) => {
   const configuration = new Configuration({
@@ -70,4 +71,49 @@ app.get("/chat", (req, res) => {
     res.json(result.data.choices[0]);
   };
   generateChat();
+});
+// -----------------------------------------------------------------cartoon quotes run here
+app.get("/mongoQuote", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/find`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "cartoonQuotes",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+    },
+  };
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
+});
+
+app.get("/mongoQuoteAdd", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/insertOne`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "cartoonQuotes",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+      document: {
+        quote: req.query.quote,
+        name: req.query.name,
+        cartoon: req.query.quote,
+      },
+    },
+  };
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
 });
