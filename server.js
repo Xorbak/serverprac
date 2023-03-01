641a6ed589ce4aaf148669f5606e7fe68c481148
dewald.labuschagne <dewaldstar@gmail.com>
2023-03-01 17:16:05 +0200

    added todo list calls

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 //allows all
 app.use(cors("*"));
 //rate limiter
-const limiter = rateLimit({ windowMs: 5 * 60 * 1000, max: 10 });
+const limiter = rateLimit({ windowMs: 5 * 60 * 1000, max: 1000 });
 app.use(limiter);
 
 app.listen(process.env.PORT || 5000, () => {
@@ -111,6 +111,120 @@ app.get("/mongoQuoteAdd", (req, res) => {
         name: req.query.name,
         cartoon: req.query.quote,
       },
+    },
+  };
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
+});
+//--------------------------------------------------------------------------todo DB
+
+/////////////////////////////////////////////////////////////add todo
+app.get("/todo", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/insertOne`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "todo",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+      document: {
+        _id: req.query._id,
+        input: req.query.input,
+        status: req.query.status,
+      },
+    },
+  };
+  axios.request(options).then((result) => {
+    console.log(req);
+    res.json(result.data);
+  });
+});
+
+///////////////////////////////////////////////////////////// add many - do I need it?
+app.get("/todoMany", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/insertMany`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "todo",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+      documents: test,
+    },
+  };
+  axios.request(options).then((result) => {
+    console.log(req);
+    res.json(result.data);
+  });
+});
+//---------------------------------------------------------------------update todo
+app.get("/todoupdate", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/updateOne`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "todo",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+      filter: { _id: req.query._id },
+      update: { $set: { status: req.query.status } },
+    },
+  };
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
+});
+
+///////////////////////////////////////////////////////// delete one
+app.get("/tododelete", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/deleteOne`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "todo",
+      database: "xorbakData",
+      dataSource: "Cluster0",
+      filter: { _id: req.query._id },
+    },
+  };
+  axios.request(options).then((result) => {
+    res.json(result.data);
+  });
+});
+app.get("/todoget", (req, res) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.REACT_APP_DBCALL}/find`,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.REACT_APP_DBAPI,
+    },
+    data: {
+      collection: "todo",
+      database: "xorbakData",
+      dataSource: "Cluster0",
     },
   };
   axios.request(options).then((result) => {
